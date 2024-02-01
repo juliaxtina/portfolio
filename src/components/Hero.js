2e6efb5824f1aa035b61346b1a1296ff8855fb05
Julia <juliacristinap.santos@gmail.com>
2024-01-31 22:04:45 -0300

    primeira alteracao

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -28,8 +28,8 @@ export function HeroBig() {
             <h1 class="max-w-2xl my-6 text-5xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-8xl dark:text-gray-100">Oi eu sou a julia</h1>
           
             <p class="max-w-2xl mb-6 mt-8 font-body text-gray-500 lg:mb-8 md:text-lg lg:text-3xl dark:text-gray-400">Aqui vai a descrição</p>
-            <a href="/about" class="text-lg inline-flex items-center justify-center font-medium text-center text-dark dark:text-gray-400">
-            o que quer saber?
+            <a href="/about" class="text-lg inline-flex items-center justify-center font-medium text-center text-red-500 dark:text-gray-400">
+            o que quer saber mano?
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
            
