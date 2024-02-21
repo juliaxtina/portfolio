2ee3057743b9dc12807f5adf79635dd6393a10d5
Julia <juliacristinap.santos@gmail.com>
2024-02-20 21:33:27 -0300

    corrections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,8 +15,8 @@ export default function Footer() {
           <div class="mb-16 lg:mb-6">
             <a href="/" class="flex items-center">
               <img
-                className="size-16 w-auto transition-all duration-1000 ease-in-out dark:brightness-110 saturate-[.10] me-2 mb-1 dark:invert"
-                src="https://www.packit.eu/wp-content/uploads/2019/08/julia-logo-pms-5255-c.png"
+                className="size-16 w-auto transition-all duration-1000 ease-in-out me-2 mb-1 "
+                src="/img/photos/flaticonlogo.png"
                 alt=""
               />
               <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
