db138ea3f7a30d993615927dd71c10b030469294
Julia <juliacristinap.santos@gmail.com>
2024-02-21 16:55:34 -0300

    new colors

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -19,21 +19,21 @@ export default function Feature() {
       </div>
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div>
-              <div class="flex justify-center items-center mb-4 w-20 h-20 rounded-full lg:h-20 lg:w-20 bg-emerald-200">
+              <div class="flex justify-center items-center mb-4 w-20 h-20 rounded-full lg:h-20 lg:w-20 bg-fuchsia-200">
               <CodeBracketIcon className="h-8 w-8" aria-hidden="true" />
               </div>
               <h3 class="mb-2 text-xl font-bold dark:text-white">UX/UI Design</h3>
               <p class="text-gray-500 dark:text-gray-400 text-balance"> Creating user-friendly designs to improve your digital presence.</p>
           </div>
           <div>
-              <div class="flex justify-center items-center mb-4 w-20 h-20 rounded-full bg-primary-100 lg:h-20 lg:w-20 bg-teal-200">
+              <div class="flex justify-center items-center mb-4 w-20 h-20 rounded-full bg-primary-100 lg:h-20 lg:w-20 bg-fuchsia-400">
               <PaintBrushIcon className="h-8 w-8" aria-hidden="true" />
               </div>
               <h3 class="mb-2 text-xl font-bold dark:text-white">Visual Design</h3>
               <p class="text-gray-500 dark:text-gray-400 text-balance"> I can offer eye-catching visual design solutions.</p>
           </div>
           <div>
-              <div class="flex justify-center items-center mb-4 w-20 h-20 rounded-full lg:h-20 lg:w-20 bg-emerald-200">
+              <div class="flex justify-center items-center mb-4 w-20 h-20 rounded-full lg:h-20 lg:w-20 bg-indigo-400">
               <VideoCameraIcon className="h-8 w-8" aria-hidden="true" />
               </div>
               <h3 class="mb-2 text-xl font-bold dark:text-white">Motion Design</h3>
